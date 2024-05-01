c845f519bfdcfa55e854783002649b1ee4f71a9b
yahyazawadi <yahyazawadi278@gmail.com>
2024-05-01 20:27:02 +0300

    yahya_amoudi

---
+++ src/components/navBarPages/LogIn.js
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+      <div>
+        <header>
+          <p>This is a Login</p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
---
+++ src/components/navBarPages/Order.js
@@ -0,0 +1,12 @@
+function Order() {
+    return (
+      <div>
+        <header>
+          <p>This is a orderpage</p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Order;
+  
