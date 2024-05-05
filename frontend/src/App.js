216fa49554d3ac471303206b181f9ecc07253513
unknown <saifalaa.099@gmail.com>
2024-05-05 15:05:56 +0300

    daily merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopBar from './components/common/TopBar';
@@ -7,21 +6,11 @@ import ContactPage from './components/ContactUsPage/ContactPage'; // Make sure t
 import HomePage from './components/Home/HomePage';
 import ServicesPage from './components/ServicesPage/ServicesPage';
 import Order from "./components/TaxiRequest/TaxiRequest";
-import Login from "./components/Login/Login"
-import { useEffect,useState } from 'react';
+import Login from "./components/Login/Login";
+import RegisterForm from "./components/CreateProfile/CreateProfile";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 
 function App() {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the backend when the component mounts
-    fetch('http://localhost:3001/backend/data')
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
   return (
     <Router>
       <div>
@@ -33,9 +22,11 @@ function App() {
         <Route path="/order" Component={Order} />
         <Route path="/services" Component={ServicesPage} />
         <Route path="/login" Component={Login} />
+        <Route path="/create-profile" Component={RegisterForm} />
+        <Route path="/forgot-password" Component={ForgotPassword} />
         <Route path="/contact" Component={ContactPage} /> </Routes>
 
-        
+
       </div>
     </Router>
   );
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,7 +1,7 @@
-// Login.js
 import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../../assets/home_pic.png';
 
 
 const Login = () => {
@@ -15,36 +15,42 @@ const Login = () => {
       setError('Please enter both username and password');
       return;
     }
-    else {setError(null);}
+    else { setError(null); }
   };
 
   return (
+    <header className="masthead">
     <div className="login-container">
-      <h2>Login</h2>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit" className="submit-button">Login</button>
-      </form>
-      <p>
-        <Link to="/forgot-password">Forgot Password?</Link>
-      </p>
-      <hr/>
-      <p>
-        <Link to="/create-profile">
-          <button className="Create-button">Create Profile</button>
-        </Link>
-      </p>
-    </div>
+        <div className='Log'>
+          <h2>Login</h2>
+          {error && <p className="error-message">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Username:</label>
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="submit-button">Login</button>
+          </form>
+          <p>
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </p>
+          <hr />
+          <p>
+            <Link to="/create-profile">
+              <button className="RegisterForm">Create Profile</button>
+            </Link>
+          </p>
+          <div />
+        </div> 
+      </div>
+      <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+
+    </header>
   );
 };
+
 export default Login;
-
-
