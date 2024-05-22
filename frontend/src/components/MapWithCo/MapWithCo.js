03146521ae7651567c307d38c943c7d2cfabffd0
unknown <saifalaa.099@gmail.com>
2024-05-22 16:50:34 +0300

    merging the map updates

--- frontend/src/components/MapWithCo/MapWithCo.js
+++ frontend/src/components/MapWithCo/MapWithCo.js
@@ -1,9 +1,11 @@
+// MapWithCo.js
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import placeholder from '../../assets/placeholder.png';
-import "./MapWithCo.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MapWithCo.css';
 
 const customIcon = new L.Icon({
   iconUrl: placeholder,
@@ -16,7 +18,7 @@ const MapWithCo = () => {
   useEffect(() => {
     const fetchPositions = async () => {
       try {
-        const response = await fetch('http://localhost:3000/positions'); // Replace with your actual backend endpoint
+        const response = await fetch('http://localhost:3000/positions'); // Replace with your actual endpoint
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -35,17 +37,19 @@ const MapWithCo = () => {
   }, []);
 
   return (
-    <MapContainer center={[32.22115149325139, 35.26074000763494]} zoom={13} scrollWheelZoom={false} className="leaflet-container">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {positions.map((position, index) => (
-        <Marker key={index} position={[position.latitude, position.longitude]} icon={customIcon}>
-          <Popup>Timestamp: {new Date(position.timestamp).toLocaleString()}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div className="container-fluid p-0 map-container">
+      <MapContainer center={[32.22115149325139, 35.26074000763494]} zoom={13} scrollWheelZoom={false} className="leaflet-container">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {positions.map((position, index) => (
+          <Marker key={index} position={[position.latitude, position.longitude]} icon={customIcon}>
+            <Popup>Timestamp: {new Date(position.timestamp).toLocaleString()}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
 
--- frontend/src/components/common/NavigationBar.js
+++ frontend/src/components/common/NavigationBar.js
@@ -38,5 +38,3 @@ function NavigationBar() {
 }
 
 export default NavigationBar;
-
-
