e30e6cc49e53159911ced7f499e1c8dc42b94d8d
ali0yaseen <152719611+ali0yaseen@users.noreply.github.com>
2024-05-28 19:07:42 +0300

    Add background to Forgot Password and Reset Password pages

---
+++ Desktop/TAXI PLUS/frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopBar from './components/common/TopBar';
+import NavigationBar from './components/common/NavigationBar';
+import ContactPage from './components/ContactUsPage/ContactPage'; 
+import HomePage from './components/Home/HomePage';
+import ServicesPage from './components/ServicesPage/ServicesPage';
+import Order from "./components/TaxiRequest/TaxiRequest";
+import Login from "./components/Login/Login";
+import RegisterForm from "./components/CreateProfile/CreateProfile";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
+import ResetPassword from "./components/ResetPassword/ResetPassword"; // Import the ResetPassword component
+import Map from "./components/Map/Map";
+import MapWithCo from "./components/MapWithCo/MapWithCo";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <TopBar />
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-profile" element={<RegisterForm />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} /> {/* Add this route */}
+          <Route path="/contact" element={<ContactPage />} /> 
+          <Route path="/map" element={<Map />} />
+          <Route path="/mapwithco" element={<MapWithCo />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
